e5b54cded82f0a8f8296e7376e1a2e46a826385b
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-05-28 15:17:18 -0700

    deploy attempt 1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/library_app");
+mongoose.connect( process.env.MONGOLAB_URI ||
+               process.env.MONGOHQ_URL || 
+               "mongodb://localhost/sample_atlas")
 
 module.exports.User = require("./user");
-module.exports.Book = require("./book");
